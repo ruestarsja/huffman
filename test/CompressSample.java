1e52760f99c9c1d6e12c85d897df735e5ba63e05
Rue Starsja <ruestarsja@gmail.com>
2025-08-15 13:34:48 -0400

    Rename 'testFiles' to 'sample' and make new 'test' directory with basic test files

---
+++ test/CompressSample.java
@@ -0,0 +1,14 @@
+package test;
+
+public class CompressSample {
+
+    // runs in vscode/vscodium with the project folder 'huffman' open
+
+    public static void main(String[] args) {
+        String[] callArgs = {
+            "./sample/sample.txt"
+        };
+        src.Main.main(callArgs);
+    }
+
+}
---
+++ test/compress-sample.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# run from the project directory (parent directory of 'test' and 'build')
+
+java -jar ./build/huffman_v1_0_0.jar ./sample/sample.txt
